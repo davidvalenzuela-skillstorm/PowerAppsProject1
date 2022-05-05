6cb4b19eb72457e0563e4c3c90072ffc57e1a71d
David <dvalenzuela@skillstorm.com>
2022-05-05 14:17:32 -0700

    Created database
    - Fixed up models for setting foreign keys and using datatypes that transfer over to SQL
    - Modified auto-generated migration to allow some fields to be nullable
    - Removed default weather forecast model and controller

--- Power Apps Project 1/Control Tower/Models/Flight.cs
+++ Power Apps Project 1/Control Tower/Models/Flight.cs
@@ -3,13 +3,12 @@
     public class Flight
     {
         public int ID { get; set; }
-        public int FlightNumber { get; set; }
-        public DateOnly DepartureDate { get; set; }
-        public TimeOnly DepartureTime { get; set; }
-        public DateOnly ArrivalDate { get; set; }
-        public TimeOnly ArrivalTime { get; set; }
+        public DateTime DepartureDateTime { get; set; }
+        public DateTime ArrivalDateTime { get; set; }
         public string DepartureAirport { get; set; } = String.Empty;
         public string ArrivalAirport { get; set; } = String.Empty;
         public int PassengerLimit { get; set; }
+
+        public ICollection<Passenger> Passenger { get; set; } = null!;
     }
 }
--- Power Apps Project 1/Control Tower/Models/Passenger.cs
+++ Power Apps Project 1/Control Tower/Models/Passenger.cs
@@ -7,6 +7,8 @@
         public string Job { get; set; } = String.Empty;
         public string Email { get; set; } = String.Empty;
         public int Age { get; set; }
-        public int BookingNumber { get; set; }
+        public int FlightID { get; set; }
+
+        public ICollection<Flight> Flight { get; set; } = null!;
     }
 }
