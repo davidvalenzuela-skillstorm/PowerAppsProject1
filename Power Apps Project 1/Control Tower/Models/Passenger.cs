5b6cba9119fc249f2f137392fa92430459c5f058
David <dvalenzuela@skillstorm.com>
2022-05-05 11:27:28 -0700

    Created "Passenger" model

---
+++ Power Apps Project 1/Control Tower/Models/Passenger.cs
@@ -0,0 +1,12 @@
+﻿namespace Control_Tower.Models
+{
+    public class Passenger
+    {
+        public int ID { get; set; }
+        public string Name { get; set; } = String.Empty;
+        public string Job { get; set; } = String.Empty;
+        public string Email { get; set; } = String.Empty;
+        public int Age { get; set; }
+        public int BookingNumber { get; set; }
+    }
+}
