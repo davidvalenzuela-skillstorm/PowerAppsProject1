882080c7a18146371b06c0f9b7f7a6d803f6321a
David <dvalenzuela@skillstorm.com>
2022-05-05 11:23:45 -0700

    Created "Flight" model

---
+++ Power Apps Project 1/Control Tower/Models/Flight.cs
@@ -0,0 +1,15 @@
+﻿namespace Control_Tower.Models
+{
+    public class Flight
+    {
+        public int ID { get; set; }
+        public int FlightNumber { get; set; }
+        public DateOnly DepartureDate { get; set; }
+        public TimeOnly DepartureTime { get; set; }
+        public DateOnly ArrivalDate { get; set; }
+        public TimeOnly ArrivalTime { get; set; }
+        public string DepartureAirport { get; set; } = String.Empty;
+        public string ArrivalAirport { get; set; } = String.Empty;
+        public int PassengerLimit { get; set; }
+    }
+}
