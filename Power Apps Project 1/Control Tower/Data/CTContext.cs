b42674d400008ca9bbe725f74ad24c6c596a7a08
David <dvalenzuela@skillstorm.com>
2022-05-05 11:38:57 -0700

    Created database context class

---
+++ Power Apps Project 1/Control Tower/Data/CTContext.cs
@@ -0,0 +1,19 @@
+﻿using Control_Tower.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Control_Tower.Data
+{
+    public class CTContext : DbContext
+    {
+        public DbSet<Flight> Flights { get; set; }
+        public DbSet<Passenger> Passengers { get; set; }
+
+        public CTContext(DbContextOptions<CTContext> options) : base(options)
+        {
+        }
+
+        //protected override void OnModelCreating(ModelBuilder modelBuilder)
+        //{
+        //}
+    }
+}
